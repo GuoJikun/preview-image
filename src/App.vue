d665c80ab913423ad13e7bb57d464e2dbf9d52d4
guojikun <guojikun@y-open.com>
2020-07-21 10:11:22 +0800

    docs：更新文档

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,14 @@
                     <owl-preview-image v-model="visiable1" :src="a" width="90vw" height="90vh"></owl-preview-image>
                 </div>
             </div>
-            <div class="preview-item">
+            <!-- <div class="preview-item">
                 <h2>基础用法--设置宽高1</h2>
                 <div class="preview-item-ctx">
                     <button class="button" @click="show2">点击查看预览界面</button>
                     <button class="button" @click="switchImage">切换图片</button>
-                    <owl-preview-image v-model="visiable2" :src="b" width="90vw" height="90vh"></owl-preview-image>
+                    <owl-preview-image v-model="visiable2" :src="b"></owl-preview-image>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -42,6 +42,10 @@ export default {
             visiable1: false,
             visiable2: false,
         };
+    },
+    mounted() {
+        // alert(`${window.innerWidth}, ${window.outerWidth}`);
+        this.getScrollWidth();
     },
     methods: {
         show() {
@@ -72,6 +76,7 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
+    height: 150vh;
 }
 .preview {
     max-width: 1300px;
