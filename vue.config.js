a52a8eb2a04877696c9558a2e160023617cce282
guojikun <guojikun@y-open.com>
2020-08-19 11:21:52 +0800

    chore：vue打包时自动去除console语句

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
+const mode = process.env.NODE_ENV;
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/owl-ui/" : "/",
-    configureWebpack: {
-        output: {
-            libraryExport: "default",
-        },
+    publicPath: mode === "production" ? "/owl-ui/" : "/",
+    configureWebpack: config => {
+        config.resolve.alias.vue$ = "vue/dist/vue.esm.js";
+        config.output.libraryExport = "default";
+        if (mode === "production") {
+            // 正式发布时移除console语句
+            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+        }
     },
     css: { extract: false },
 };
