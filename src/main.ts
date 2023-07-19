ca23fce545391cb0f91843f8fc617be9df890dbc
GuoJikun <guojikun486546@163.com>
2023-07-19 15:20:39 +0800

    🔧 build: 升级依赖项

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import './assets/main.css'
 
-createApp(App).use(router).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "node:path";
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/preview-image/",
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  plugins: [vue()],
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
