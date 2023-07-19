95edfc7a41c026b84435558f1e467ed96b13a31d
GuoJikun <guojikun486546@163.com>
2023-07-19 16:47:01 +0800

    ✨ feat: 更改为setup写法

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,65 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import a from './assets/a.jpg'
+import b from './assets/logo.png'
+import { FoxPreviewImage } from './components/preview-image/index'
+
+const visible = ref<boolean>(false)
+const visible1 = ref<boolean>(false)
+
+const show = () => {
+  visible.value = true
+}
+const show1 = () => {
+  visible1.value = true
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div>
+    <div class="preview">
+      <div class="preview-item">
+        <h2>基础用法</h2>
+        <div class="preview-item-ctx">
+          <button class="button" @click="show">点击查看预览界面</button>
+          <fox-preview-image v-model="visible" :src="[a, b]" :initial-index="1"></fox-preview-image>
+        </div>
+      </div>
+      <div class="preview-item">
+        <h2>基础用法-只有一张图片时不显示左右的切换按钮</h2>
+        <div class="preview-item-ctx">
+          <button class="button" @click="show1">点击查看预览界面-没有切换按钮</button>
+          <fox-preview-image v-model="visible1" :src="[a]"></fox-preview-image>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 150vh;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.preview {
+  max-width: 1300px;
+  margin: 0 auto;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+.button {
+  background-color: #fff;
+  color: #409eff;
+  border: 1px solid #409eff;
+  line-height: 1em;
+  padding: 8px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    color: white;
+    background-color: #409eff;
   }
 }
 </style>
--- src/components/preview-image/src/index.vue
+++ src/components/preview-image/src/index.vue
@@ -5,7 +5,7 @@
         ref="refEl"
         class="owl-preview"
         :style="{
-          'z-index': zIndex,
+          'z-index': zIndex
         }"
         tabindex="1"
         @keyup.esc.exact="close"
@@ -23,25 +23,16 @@
               @mouseup="mouseup"
               @mousedown="mousedown"
               :style="{
-                transform: `rotate(${angle}deg) scale(${scale}) translate(${x}px,${y}px)`,
+                transform: `rotate(${angle}deg) scale(${scale}) translate(${x}px,${y}px)`
               }"
               style="display: inline-block"
             >
-              <img
-                class="owl-preview-image"
-                :src="item"
-                alt="被拖拽的图片"
-                draggable="false"
-              />
+              <img class="owl-preview-image" :src="item" alt="被拖拽的图片" draggable="false" />
             </div>
           </template>
         </div>
         <!-- 关闭按钮 -->
-        <div
-          class="owl-preview-close"
-          :style="{ 'z-index': zIndex + 1 }"
-          @click="close"
-        >
+        <div class="owl-preview-close" :style="{ 'z-index': zIndex + 1 }" @click="close">
           <Close />
         </div>
         <!-- 左右切换按钮 -->
@@ -50,32 +41,17 @@
           :style="{ 'z-index': zIndex + 1 }"
           v-if="uri && uri.length > 1"
         >
-          <div
-            class="owl-preview-operate-item owl-preview-operate-item-left"
-            @click="prev"
-          >
-            <ArrowRight
-              class="owl-preview-operate-icon owl-preview-operate-icon-left"
-            />
+          <div class="owl-preview-operate-item owl-preview-operate-item-left" @click="prev">
+            <ArrowRight class="owl-preview-operate-icon owl-preview-operate-icon-left" />
           </div>
           <div class="owl-preview-operate-item" @click="next">
-            <ArrowRight
-              class="owl-preview-operate-icon owl-preview-operate-icon"
-            />
+            <ArrowRight class="owl-preview-operate-icon owl-preview-operate-icon" />
           </div>
         </div>
 
         <div class="owl-preview-utils" :style="{ 'z-index': zIndex + 1 }">
-          <ZoomOut
-            title="缩小"
-            class="owl-preview-utils-item"
-            @click="zoomOut"
-          />
-          <ZoomIn
-            title="放大"
-            class="owl-preview-utils-item"
-            @click="enlarge"
-          />
+          <ZoomOut title="缩小" class="owl-preview-utils-item" @click="zoomOut" />
+          <ZoomIn title="放大" class="owl-preview-utils-item" @click="enlarge" />
 
           <div
             title="缩放倍数"
@@ -91,344 +67,307 @@
             v-text="getCurrIndex"
           ></div>
 
-          <RotateLeft
-            title="左旋转"
-            class="owl-preview-utils-item"
-            @click="clockwiseRotation"
-          />
+          <RotateLeft title="左旋转" class="owl-preview-utils-item" @click="clockwiseRotation" />
           <RotateRight
             title="右旋转"
             class="owl-preview-utils-item"
             @click="anticlockwiseRotation"
           />
-          <Download
-            title="下载/保存"
-            class="owl-preview-utils-item"
-            @click="downloadFile"
-          />
+          <Download title="下载/保存" class="owl-preview-utils-item" @click="downloadFile" />
         </div>
       </div>
     </transition>
   </teleport>
 </template>
 
-<script lang="ts">
-import types from "@/utils/types";
-import ZoomOut from "@/components/zoom-out.vue";
-import ZoomIn from "@/components/zoom-in.vue";
-import Close from "@/components/close.vue";
-import RotateLeft from "@/components/rotate-left.vue";
-import RotateRight from "@/components/rotate-right.vue";
-import ArrowRight from "@/components/arrow-right.vue";
-import Download from "@/components/download.vue";
-import {
-  defineComponent,
-  onMounted,
-  computed,
-  ref,
-  reactive,
-  watchEffect,
-} from "vue";
+<script lang="ts" setup>
+import types from '@/utils/types'
+import ZoomOut from '@/components/zoom-out.vue'
+import ZoomIn from '@/components/zoom-in.vue'
+import Close from '@/components/close.vue'
+import RotateLeft from '@/components/rotate-left.vue'
+import RotateRight from '@/components/rotate-right.vue'
+import ArrowRight from '@/components/arrow-right.vue'
+import Download from '@/components/download.vue'
+import { watch, onMounted, computed, ref, reactive, onBeforeMount } from 'vue'
 
-export default defineComponent({
-  name: "FoxPreviewImage",
-  components: {
-    ZoomOut,
-    ZoomIn,
-    RotateLeft,
-    RotateRight,
-    Close,
-    ArrowRight,
-    Download,
+defineOptions({
+  name: 'FoxPreviewImage'
+})
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false
   },
-  props: {
-    modelValue: {
-      type: Boolean,
-      default: false,
-    },
-    src: {
-      type: [String, Array],
-      required: true,
-    },
-    zIndex: {
-      type: Number,
-      default: 9000,
-    },
-    initialIndex: {
-      type: Number,
-      default: 0,
-    },
+  src: {
+    type: [String, Array],
+    required: true
   },
-  setup(props, { emit }) {
-    let bodyStyleCache = "";
-    const refEl = ref(null);
-    let flag = ref<boolean>(false);
-    let status = ref<number>(0);
-    let active =
-      props.src && props.src.length
-        ? ref<number>(props.initialIndex)
-        : ref<number>(0);
-    let angle = ref<number>(0);
-    let scale = ref<number>(1);
-    let cacheX = ref<number>(0);
-    let cacheY = ref<number>(0);
-    let x = ref<number>(0);
-    let y = ref<number>(0);
-    let uri = ref<Array<string>>([]);
-    let startLocation = reactive({
-      x: 0,
-      y: 0,
-    });
-
-    const init = () => {
-      flag.value = props.modelValue;
-    };
-    const close = () => {
-      flag.value = false;
-      emit("update:modelValue", flag.value);
-    };
-
-    const move = (e: MouseEvent) => {
-      if (status.value === 1) {
-        const { x: mouseX, y: mouseY } = e;
-        // 鼠标的移动距离
-        const mvX = mouseX - startLocation.x;
-        const mvY = mouseY - startLocation.y;
-        x.value = mvX + x.value - cacheX.value;
-        y.value = mvY + y.value - cacheY.value;
-        requestAnimationFrame(() => {
-          cacheX.value = mvX;
-          cacheY.value = mvY;
-        });
-      }
-    };
-
-    /**
-     * 缩小
-     */
-    const zoomOut = () => {
-      if (scale.value > 0.5) {
-        scale.value -= 0.1;
-      }
-    };
-    /**
-     * 放大
-     */
-    const enlarge = () => {
-      if (scale.value < 2) {
-        scale.value += 0.1;
-      }
-    };
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const mousewheel = (ev: any) => {
-      requestAnimationFrame(() => {
-        const isUp = (ev.wheelDelta || ev.detail * -40) > 0;
-        if (isUp) {
-          enlarge();
-        } else {
-          zoomOut();
-        }
-      });
-    };
-
-    const handleMouseMove = move;
-    const handleMousewheel = mousewheel;
-
-    const mouseup = () => {
-      status.value = 0;
-      cacheX.value = 0;
-      cacheY.value = 0;
-    };
-    const mousedown = (e: MouseEvent) => {
-      status.value = 1;
-      startLocation = { x: e.x, y: e.y };
-    };
-
-    /**
-     * 顺时针旋转
-     */
-    const clockwiseRotation = () => {
-      angle.value += 90;
-    };
-    /**
-     * 逆时针旋转
-     */
-    const anticlockwiseRotation = () => {
-      angle.value -= 90;
-    };
-
-    /**下载图片 */
-
-    const downloadFile = () => {
-      const cur = uri.value[active.value];
-      const a = document.createElement("a");
-      a.download = cur.split("/").at(-1) as string;
-      a.href = cur;
-      a.click();
-      a.remove();
-    };
-
-    /**
-     * 获取滚动条的宽度
-     */
-    const getScrollWidth = () => {
-      const scroll = document.createElement("div");
-      const scrollIn = document.createElement("div");
-      scroll.appendChild(scrollIn);
-      scroll.style.width = "100px";
-      scroll.style.height = "50px";
-      scroll.style.overflow = "scroll";
-      scroll.style.marginLeft = "-100000px";
-      document.body.appendChild(scroll);
-      const scrollInWidth = scrollIn.offsetWidth;
-      const scrollWidth = scroll.offsetWidth;
-      const tmp = setTimeout(() => {
-        document.body.removeChild(scroll);
-        clearTimeout(tmp);
-      }, 10);
-      return scrollWidth - scrollInWidth;
-    };
-    // 上一张图片
-    const prev = () => {
-      const len = uri.value.length || 0;
-      if (active.value > 0) {
-        active.value--;
-      } else {
-        active.value = len - 1;
-      }
-      angle.value = 0;
-      scale.value = 1;
-      x.value = 0;
-      y.value = 0;
-      startLocation.x = 0;
-      startLocation.y = 0;
-      cacheX.value = 0;
-      cacheY.value = 0;
-    };
-    // 下一张图片
-    const next = () => {
-      const len = uri.value.length || 0;
-      if (active.value < len - 1) {
-        active.value++;
-      } else {
-        active.value = 0;
-      }
-      angle.value = 0;
-      scale.value = 1;
-      x.value = 0;
-      y.value = 0;
-
-      startLocation.x = 0;
-      startLocation.y = 0;
-      cacheX.value = 0;
-      cacheY.value = 0;
-    };
-
-    const getCurrScale = computed(() => {
-      return parseFloat(scale.value.toFixed(1));
-    });
-
-    const getCurrIndex = computed(() => {
-      return `${active.value + 1}/${uri.value.length}`;
-    });
-
-    onMounted(() => {
-      init();
-    });
-
-    const hasScrollbar = (el: HTMLElement) => {
-      if (el.scrollHeight > window.innerHeight) {
-        return true;
-      }
-      return false;
-    };
-
-    watchEffect(() => {
-      flag.value = props.modelValue;
-      if (props.modelValue) {
-        if (refEl.value !== null) {
-          (refEl.value as HTMLElement).focus();
-        }
-        const isScrollBar = hasScrollbar(document.body);
-        if (isScrollBar) {
-          document.body.style.paddingRight = getScrollWidth() + "px";
-          document.body.classList.add("fox-lock-window");
-        }
-      } else {
-        document.body.classList.remove("fox-lock-window");
-        document.body.style.paddingRight = "0px";
-      }
-    });
-
-    watchEffect(() => {
-      const type = types(props.src);
-      if (type === "string") {
-        active.value = 0;
-        angle.value = 0;
-        scale.value = 1;
-        x.value = 0;
-        y.value = 0;
-        startLocation.x = 0;
-        startLocation.y = 0;
-        cacheX.value = 0;
-        cacheY.value = 0;
-
-        uri.value = [props.src as string];
-      } else if (type === "array") {
-        if (props.initialIndex >= 0 && props.initialIndex < props.src.length) {
-          active.value = props.initialIndex;
-        } else {
-          active.value = 0;
-        }
-        uri.value = props.src as string[];
-        angle.value = 0;
-        scale.value = 1;
-        x.value = 0;
-        y.value = 0;
-        startLocation.x = 0;
-        startLocation.y = 0;
-        cacheX.value = 0;
-        cacheY.value = 0;
-        uri.value = props.src as string[];
-      }
-    });
-
-    return {
-      refEl,
-      flag,
-      status,
-      active,
-      uri,
-      scale,
-      angle,
-      x,
-      y,
-      handleMouseMove,
-      handleMousewheel,
-      /**methods */
-      close,
-      prev,
-      next,
-      mouseup,
-      mousedown,
-      zoomOut,
-      enlarge,
-      anticlockwiseRotation,
-      clockwiseRotation,
-      /**computed */
-      getCurrScale,
-      getCurrIndex,
-      downloadFile,
-    };
+  zIndex: {
+    type: Number,
+    default: 9000
   },
-});
+  initialIndex: {
+    type: Number,
+    default: 0
+  }
+})
+const emit = defineEmits(['update:modelValue'])
+
+let bodyStyleCache = ''
+
+onBeforeMount(() => {
+  bodyStyleCache = document.body.style.cssText
+  console.log(bodyStyleCache)
+})
+
+const refEl = ref(null)
+let flag = ref<boolean>(false)
+let status = ref<number>(0)
+let active = props.src && props.src.length ? ref<number>(props.initialIndex) : ref<number>(0)
+let angle = ref<number>(0)
+let scale = ref<number>(1)
+let cacheX = ref<number>(0)
+let cacheY = ref<number>(0)
+let x = ref<number>(0)
+let y = ref<number>(0)
+let uri = ref<Array<string>>([])
+let startLocation = reactive({
+  x: 0,
+  y: 0
+})
+
+const init = () => {
+  flag.value = props.modelValue
+}
+const close = () => {
+  flag.value = false
+  emit('update:modelValue', flag.value)
+}
+
+const move = (e: MouseEvent) => {
+  if (status.value === 1) {
+    const { x: mouseX, y: mouseY } = e
+    // 鼠标的移动距离
+    const mvX = mouseX - startLocation.x
+    const mvY = mouseY - startLocation.y
+    x.value = mvX + x.value - cacheX.value
+    y.value = mvY + y.value - cacheY.value
+    requestAnimationFrame(() => {
+      cacheX.value = mvX
+      cacheY.value = mvY
+    })
+  }
+}
+
+/**
+ * 缩小
+ */
+const zoomOut = () => {
+  if (scale.value > 0.5) {
+    scale.value -= 0.1
+  }
+}
+/**
+ * 放大
+ */
+const enlarge = () => {
+  if (scale.value < 2) {
+    scale.value += 0.1
+  }
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mousewheel = (ev: any) => {
+  requestAnimationFrame(() => {
+    const isUp = (ev.wheelDelta || ev.detail * -40) > 0
+    if (isUp) {
+      enlarge()
+    } else {
+      zoomOut()
+    }
+  })
+}
+
+const handleMouseMove = move
+const handleMousewheel = mousewheel
+
+const mouseup = () => {
+  status.value = 0
+  cacheX.value = 0
+  cacheY.value = 0
+}
+const mousedown = (e: MouseEvent) => {
+  status.value = 1
+  startLocation = { x: e.x, y: e.y }
+}
+
+/**
+ * 顺时针旋转
+ */
+const clockwiseRotation = () => {
+  angle.value += 90
+}
+/**
+ * 逆时针旋转
+ */
+const anticlockwiseRotation = () => {
+  angle.value -= 90
+}
+
+/**下载图片 */
+
+const downloadFile = () => {
+  const cur = uri.value[active.value]
+  const a = document.createElement('a')
+  const tmp = cur.split('/')
+  a.download = tmp[tmp.length - 1] as string
+  a.href = cur
+  a.click()
+  a.remove()
+}
+
+/**
+ * 获取滚动条的宽度
+ */
+const getScrollWidth = () => {
+  const scroll = document.createElement('div')
+  const scrollIn = document.createElement('div')
+  scroll.appendChild(scrollIn)
+  scroll.style.width = '100px'
+  scroll.style.height = '50px'
+  scroll.style.overflow = 'scroll'
+  scroll.style.marginLeft = '-100000px'
+  document.body.appendChild(scroll)
+  const scrollInWidth = scrollIn.offsetWidth
+  const scrollWidth = scroll.offsetWidth
+  const tmp = setTimeout(() => {
+    document.body.removeChild(scroll)
+    clearTimeout(tmp)
+  }, 10)
+  return scrollWidth - scrollInWidth
+}
+// 上一张图片
+const prev = () => {
+  const len = uri.value.length || 0
+  if (active.value > 0) {
+    active.value--
+  } else {
+    active.value = len - 1
+  }
+  angle.value = 0
+  scale.value = 1
+  x.value = 0
+  y.value = 0
+  startLocation.x = 0
+  startLocation.y = 0
+  cacheX.value = 0
+  cacheY.value = 0
+}
+// 下一张图片
+const next = () => {
+  const len = uri.value.length || 0
+  if (active.value < len - 1) {
+    active.value++
+  } else {
+    active.value = 0
+  }
+  angle.value = 0
+  scale.value = 1
+  x.value = 0
+  y.value = 0
+
+  startLocation.x = 0
+  startLocation.y = 0
+  cacheX.value = 0
+  cacheY.value = 0
+}
+
+const getCurrScale = computed(() => {
+  return parseFloat(scale.value.toFixed(1))
+})
+
+const getCurrIndex = computed(() => {
+  return `${active.value + 1}/${uri.value.length}`
+})
+
+onMounted(() => {
+  init()
+})
+
+const hasScrollbar = (el: HTMLElement) => {
+  if (el.scrollHeight > window.innerHeight) {
+    return true
+  }
+  return false
+}
+
+watch(
+  () => props.modelValue,
+  (val) => {
+    flag.value = val
+    if (val) {
+      if (refEl.value !== null) {
+        ;(refEl.value as HTMLElement).focus()
+      }
+      const isScrollBar = hasScrollbar(document.body)
+      if (isScrollBar) {
+        document.body.style.paddingRight = getScrollWidth() + 'px'
+        document.body.classList.add('fox-lock-window')
+      }
+    } else {
+      document.body.classList.remove('fox-lock-window')
+      if (bodyStyleCache) {
+        document.body.style.cssText = bodyStyleCache
+      } else {
+        document.body.removeAttribute('style')
+      }
+    }
+  }
+)
+
+watch(
+  () => props.src,
+  (val) => {
+    const type = types(val)
+    if (type === 'string') {
+      active.value = 0
+      angle.value = 0
+      scale.value = 1
+      x.value = 0
+      y.value = 0
+      startLocation.x = 0
+      startLocation.y = 0
+      cacheX.value = 0
+      cacheY.value = 0
+
+      uri.value = [val as string]
+    } else if (type === 'array') {
+      if (props.initialIndex >= 0 && props.initialIndex < val.length) {
+        active.value = props.initialIndex
+      } else {
+        active.value = 0
+      }
+      uri.value = val as string[]
+      angle.value = 0
+      scale.value = 1
+      x.value = 0
+      y.value = 0
+      startLocation.x = 0
+      startLocation.y = 0
+      cacheX.value = 0
+      cacheY.value = 0
+      uri.value = val as string[]
+    }
+  }
+)
 </script>
 
 <style lang="scss">
 .owl-preview {
   width: 100vw;
   height: 100vh;
-  position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
   margin-left: -50vw;
@@ -535,7 +474,7 @@ export default defineComponent({
         position: absolute;
         width: 100%;
         height: 100%;
-        content: "";
+        content: '';
         z-index: 1;
         border: 1px solid #606266;
         box-sizing: border-box;
