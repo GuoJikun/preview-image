9fb20709e1706a41995436150923fc5440114223
GuoJikun <guojikun486546@163.com>
2025-08-18 11:31:30 +0800

    feat: 优化 tsconfig 中 vue jsx 配置

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,19 @@
-export type ToolType = 'zoom-out' | 'zoom-in' | 'contraRotate' | 'clockwiseRotation' | 'download'
+export type ToolType =
+  | 'zoom-out'
+  | 'zoom-in'
+  | 'contraRotate'
+  | 'clockwiseRotation'
+  | 'download'
 
 export const downloadFile = (url: string, name: string) => {
   const a = document.createElement('a')
   a.download = name
   a.href = url
   a.style.display = 'none'
-  document.body.appendChild(a)
+  document.body.append(a)
   a.click()
   const timer = setTimeout(() => {
-    document.body.removeChild(a)
+    a.remove()
     clearTimeout(timer)
   }, 10)
 }
@@ -19,16 +24,16 @@ export const downloadFile = (url: string, name: string) => {
 export const getScrollWidth = () => {
   const scroll = document.createElement('div')
   const scrollIn = document.createElement('div')
-  scroll.appendChild(scrollIn)
+  scroll.append(scrollIn)
   scroll.style.width = '100px'
   scroll.style.height = '50px'
   scroll.style.overflow = 'scroll'
   scroll.style.marginLeft = '-100000px'
-  document.body.appendChild(scroll)
+  document.body.append(scroll)
   const scrollInWidth = scrollIn.offsetWidth
   const scrollWidth = scroll.offsetWidth
   const tmp = setTimeout(() => {
-    document.body.removeChild(scroll)
+    scroll.remove()
     clearTimeout(tmp)
   }, 10)
   return scrollWidth - scrollInWidth
@@ -36,7 +41,9 @@ export const getScrollWidth = () => {
 
 export function type<T>(value: T): string {
   const target = Object.prototype.toString.call(value)
-  const type = target.replace(new RegExp('(^[[a-z]+ )([A-Za-z]+)(])', 'g'), '$2').toLowerCase()
+  const type = target
+    .replaceAll(/(^[[a-z]+ )([A-Za-z]+)(\])/g, '$2')
+    .toLowerCase()
   return type
 }
 
@@ -67,14 +74,15 @@ function isFalse<T extends never>(val: T): boolean {
   return [0, undefined, null].includes(val)
 }
 
-prototype['isObject'] = isObject
-prototype['isFunction'] = isFunction
-prototype['isArray'] = isArray
-prototype['isSymbol'] = isSymbol
-prototype['isFalse'] = isFalse
+prototype.isObject = isObject
+prototype.isFunction = isFunction
+prototype.isArray = isArray
+prototype.isSymbol = isSymbol
+prototype.isFalse = isFalse
 
 type.prototype = prototype
 
-export default type
+export { isArray, isFunction, isObject, isSymbol }
 
-export { isObject, isFunction, isArray, isSymbol }
+// eslint-disable-next-line import/no-default-export
+export default type
